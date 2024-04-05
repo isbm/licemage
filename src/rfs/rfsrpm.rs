cc34d3fe77499c900d5cdf04ce2baa044ff0c59d
Bo Maryniuk <bo@maryniuk.net>
2024-04-05 15:02:55 +0000

    Add RPM stubs

--- src/rfs/rfsrpm.rs
+++ src/rfs/rfsrpm.rs
@@ -6,6 +6,11 @@ impl RpmRootFsScan {}
 
 impl RootFSItf for RpmRootFsScan {
     fn get_pkg_list(&self) -> Vec<String> {
-        todo!("Go ahead, implement this for RPM and send us PR! :-)")
+        todo!("Not implemented")
+    }
+
+    #[allow(unused_variables)]
+    fn get_pkg_license(&self, pkgname: String) -> crate::licences::PkgLicence {
+        todo!("Not implemented")
     }
 }
