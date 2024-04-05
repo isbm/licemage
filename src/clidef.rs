fa49cbc5ebef9aef15019aa4b8a5436227092746
Bo Maryniuk <bo@maryniuk.net>
2024-04-05 15:01:23 +0000

    Update cli definition

--- src/clidef.rs
+++ src/clidef.rs
@@ -5,14 +5,8 @@ pub fn cli(version: &'static str) -> Command {
     Command::new("licemage")
         .version(version)
         .about("licemage - image license scanner")
-        .arg(
-            Arg::new("path")
-                .short('p')
-                .long("path")
-                .required(true)
-                .help("Path to a mounted rootfs"),
-        )
-        .arg(Arg::new("claimed").short('c').long("claimed").help(format!(
+        .arg(Arg::new("path").short('p').long("path").required(true).help("Path to a mounted rootfs"))
+        .arg(Arg::new("claimed").short('c').long("claimed").action(clap::ArgAction::SetTrue).help(format!(
             "Extract only published licences, provided by a package metadata.\n{}",
             "WARNING: package metadata might be inaccurate!\n".bright_yellow()
         )))
@@ -20,12 +14,8 @@ pub fn cli(version: &'static str) -> Command {
             Arg::new("examine")
                 .short('e')
                 .long("examine")
+                .action(clap::ArgAction::SetTrue)
                 .help("Extract sources for all installed packages and find their licenses"),
         )
-        .arg(
-            Arg::new("temp")
-                .short('t')
-                .long("temp")
-                .help("Specify other location for caching temporary data"),
-        )
+        .arg(Arg::new("temp").short('t').long("temp").help("Specify other location for caching temporary data"))
 }
