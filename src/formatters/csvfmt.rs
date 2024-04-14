4852d005825ad9157d9a9951dd723031ac47b7e5
Bo Maryniuk <bo@maryniuk.net>
2024-04-14 17:02:59 +0000

    Implement CSV formatter

---
+++ src/formatters/csvfmt.rs
@@ -0,0 +1,54 @@
+use super::stdfmt::DataFormatter;
+use crate::rfs::RfsScan;
+use std::io::{self, BufWriter};
+
+pub struct CSVDataFormatter<'a> {
+    rfs: &'a RfsScan,
+}
+
+impl<'a> CSVDataFormatter<'a> {
+    fn vec2csv(&self, data: &Vec<String>) -> String {
+        if data.is_empty() {
+            return "".to_string();
+        }
+
+        let mut out = String::new();
+        for mut e in data {
+            if e.is_empty() {
+                continue;
+            }
+
+            if e.contains(' ') {
+                out.push_str(format!(",\"{}\"", e).as_str());
+            } else {
+                out.push_str(format!(",{}", e).as_str());
+            }
+        }
+
+        out
+    }
+}
+
+impl<'a> DataFormatter<'a> for CSVDataFormatter<'_> {
+    fn format(&self) -> String {
+        let mut out = String::from("Package,Licences\n");
+        for pkn in self.rfs.get_pkg_list() {
+            let pkl = self.rfs.get_pkg_license(pkn.to_owned());
+            out.push_str(pkn.as_str());
+            if !pkl.get_id().is_empty() {
+                out.push_str(format!(",{}", pkl.get_id()).as_str());
+            }
+            let otr = self.vec2csv(pkl.get_other());
+            if !otr.is_empty() {
+                out.push_str(&otr);
+            }
+            out.push_str("\n");
+        }
+
+        out
+    }
+
+    fn new(rfs: &'a RfsScan) -> Box<(dyn DataFormatter + 'a)> {
+        Box::new(CSVDataFormatter { rfs })
+    }
+}
